67ec32497d3486f4490cdae863cef3cce46ffa99
yanhao <yanhao@xiaomi.com>
2012-04-18 12:24:40 +0800

    add makefiel or something
    Change-Id: I48096f695e15f312a0b25de0258f04d3ea7b9255

--- mx_update.sh
+++ mx_update.sh
@@ -1,34 +1,59 @@
 #! /bin/bash
-#must got root permission before upgrade
+#make clean
 
-make clean
+#make fullota
 
-make zipfile
+adb shell mv /data/local/tmp /data/local/tmp.bak
+adb shell ln -s /data /data/local/tmp
+adb reboot
+adb wait-for-device
+adb shell rm /data/local.prop > /dev/null
+adb shell "echo \"ro.kernel.qemu=1\" > /data/local.prop"
+adb reboot
+adb wait-for-device
+adb shell id
 if [ -d out/temp ];then
 	rm -rf out/temp
 fi
-unzip > /dev/null out/MIUI_mx.zip -d out/temp
+
+unzip > /dev/null out/fullota.zip -d out/temp
 adb remount
-#adb shell su
-#adb shell mount -o remount,rw /system
-#adb push temp/system/app/ /system/app
-#adb push temp/system/framework /system/framework
+adb shell stop
+
 DIR_PREFIX=out/temp/system
+
+adb shell rm /system/app/* > /dev/null
+adb shell rm /system/framework/*.odex > /dev/null
+
 adb push $DIR_PREFIX/app /system/app
 adb push $DIR_PREFIX/framework/framework.jar /system/framework/framework.jar
 adb push $DIR_PREFIX/framework/android.policy.jar /system/framework/android.policy.jar
 adb push $DIR_PREFIX/framework/services.jar /system/framework/services.jar
 adb push $DIR_PREFIX/framework/miui-framework.jar /system/framework/miui-framework.jar
+adb push $DIR_PREFIX/build.prop  /system/build.prop
+
 
 adb push $DIR_PREFIX/framework/framework-miui-res.apk /system/framework/framework-miui-res.apk
 adb push $DIR_PREFIX/framework/framework-res.apk /system/framework/framework-res.apk
 
+adb push $DIR_PREFIX/etc/apns-conf.xml /system/etc/apns-conf.xml
+adb push $DIR_PREFIX/etc/telocation.db /system/etc/telocation.db
+adb push $DIR_PREFIX/etc/yellowpage.db /system/etc/yellowpage.db
+
+adb push $DIR_PREFIX/lib/content-types.properties /system/lib/content-types.properties
 #push media for changing some resources
 adb push $DIR_PREFIX/media /system/media
 #push miui-framework.xml for the permission of miui-framework.jar
+adb push $DIR_PREFIX/etc/license /system/etc/license
 adb push $DIR_PREFIX/etc/permissions/miui-framework.xml /system/etc/permissions/miui-framework.xml 
-#push invoke-as for fix MIUI-home bug
-#push su,busybox, dexdump?
-adb push $DIR_PREFIX/xbin /system/xbin
 adb reboot
+adb wait-for-device
+adb shell start
+adb shell rm /data/local/tmp
+adb shell rm /data/local.prop
+adb shell mv /data/local/tmp.bak /data/local/tmp
+adb shell "wipe data"
+echo "update ok after reboot"
+adb reboot
+
 
