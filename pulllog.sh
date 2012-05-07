fea646f4d90fc316a23e7f2d8786a6bbfc79b42b
wangwenjun <wangwenjun@xiaomi.com>
2012-05-07 21:26:09 +0800

    Revert "a++"add other groups exec permission""
    This reverts commit da4f2bf837a02a93714ab8fa4ed27fb0c9f1c037

---
+++ pulllog.sh
@@ -0,0 +1,43 @@
+#! /bin/bash
+#
+#pull log from /data/system/dropbox
+
+ROOT_END=root_end
+ROOT_START=root_start
+DATA_FORMAT=dataformat.sh
+LOG_DIR=log
+OLD_PWD=`pwd`
+LOG_FILE=${LOG_DIR}/${LOG_DIR}-`date +"%Y%m%d-%H%M"`
+
+if [ ! -d $LOG_DIR ]; then
+	mkdir $LOG_DIR
+	test $? != 0 && exit
+fi
+
+if [ ! -d $LOG_FILE ]; then
+	mkdir $LOG_FILE
+	test $? != 0 && exit
+fi
+
+adb wait-for-device
+echo `adb shell id` | grep "uid=0"
+
+if [ $? != 0 ]; then
+	test ! -e $ROOT_START && exit
+	. ${ROOT_START}
+fi
+
+adb pull /data/system/dropbox ${LOG_FILE}
+adb shell rm /data/system/dropbox/*
+
+test ! -e $DATA_FORMAT
+. ${DATA_FORMAT} ${LOG_FILE}
+
+echo "creat dir $LOG_FILE"
+
+cd $OLD_PWD
+
+test ! -e $ROOT_END && exit
+. ${ROOT_END}
+
+
