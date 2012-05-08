f0167657e82adc4593e7c4c7b8d61d255576bf80
yanhao <yanhao@xiaomi.com>
2012-05-08 16:13:14 +0800

    fix update ota package bug
    Change-Id: Id14bb1beb1469a3eb9e61ce6b6484146945b75ab

--- releasetools.py
+++ releasetools.py
@@ -9,10 +9,7 @@ def AddAssertions(info):
         if "unmount(\"/system\")" in edify.script[i]:
             edify.script[i] = edify.script[i].replace("unmount", 'ui_print')
         elif "format" in edify.script[i] and "/dev/block/mmcblk0p2" in edify.script[i]:
-            edify.script[i] = edify.script[i].replace("format", 'mount')
-            edify.script[i] = edify.script[i].replace("\"0\"", '"/system"')
-        elif "mount(\"ext4\", \"EMMC\", \"/dev/block/mmcblk0p2\", \"/system\");" in edify.script[i]:
-            edify.script[i] = edify.script[i].replace("mount(\"ext4\", \"EMMC\", \"/dev/block/mmcblk0p2\", \"/system\");", 'delete_recursive("/system/app", "/system/bin", "/system/etc", "/system/fonts", "/system/framework", "/system/lib", "/system/media", "/system/tts", "/system/usr", "/system/vendor", "/system/xbin","/system/build.prop", "0");')
+            edify.script[i] = edify.script[i].replace("format(\"ext4\", \"EMMC\", \"/dev/block/mmcblk0p2\", \"0\");", 'delete_recursive("/system/app", "/system/bin", "/system/etc", "/system/fonts", "/system/framework", "/system/lib", "/system/media", "/system/tts", "/system/usr", "/system/vendor", "/system/xbin","/system/build.prop", "0");')
     return 
 
 def FullOTA_InstallEnd(info):
